7e11dc354ace25ec87e61b61171cf8d3d7de4118
root <root@localhost.localdomain>
2026-02-23 04:27:39 -0500

    Add database.sql

---
+++ database.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE coffee_pos;
+USE coffee_pos;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    username VARCHAR(50) UNIQUE,
+    password VARCHAR(255),
+    role ENUM('admin','cashier') DEFAULT 'cashier',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (name,username,password,role)
+VALUES ('Admin','admin',MD5('123'),'admin');
+
+CREATE TABLE suppliers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    supplier_name VARCHAR(150),
+    phone VARCHAR(50),
+    address TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(150),
+    category VARCHAR(100),
+    buy_price DECIMAL(10,2),
+    sell_price DECIMAL(10,2),
+    stock INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE invoices (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    invoice_no VARCHAR(50),
+    total DECIMAL(10,2),
+    paid DECIMAL(10,2),
+    balance DECIMAL(10,2),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE invoice_items (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    invoice_id INT,
+    product_id INT,
+    qty INT,
+    price DECIMAL(10,2)
+);
