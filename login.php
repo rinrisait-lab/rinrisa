39548c3017d427dccfc56cd82d3f01a9091ffa6a
rinrisait-lab <rinrisait@gmail.com>
2026-02-24 10:53:46 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,16 +1,22 @@
 <?php
 session_start();
-require 'functions.php';
-
-if (isLoggedIn()) {
-    header("Location: index.php");
-    exit;
-}
+require 'config.php';
 
 $error = '';
 
 if (isset($_POST['login'])) {
-    if (login($_POST['username'], $_POST['password'])) {
+
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = md5($_POST['password']);
+
+    $query = mysqli_query($conn,
+        "SELECT * FROM users WHERE username='$username' AND password='$password'"
+    );
+
+    if (mysqli_num_rows($query) > 0) {
+        $user = mysqli_fetch_assoc($query);
+        $_SESSION['user'] = $user['username'];
+
         header("Location: index.php");
         exit;
     } else {
@@ -22,16 +28,19 @@ if (isset($_POST['login'])) {
 <!DOCTYPE html>
 <html>
 <head>
-<title>Login - POS</title>
-<link rel="stylesheet" href="style.css">
+<title>Login</title>
 </head>
 <body>
+
 <h2>Login</h2>
-<?php if($error) echo "<p>$error</p>"; ?>
+
+<?php if($error) echo "<p style='color:red'>$error</p>"; ?>
+
 <form method="post">
 <input type="text" name="username" required>
 <input type="password" name="password" required>
 <button name="login">Login</button>
 </form>
+
 </body>
 </html>
