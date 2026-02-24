9b5815431f5b3fd54bd39186275f0932658ed4e0
rinrisait-lab <rinrisait@gmail.com>
2026-02-24 10:11:40 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,19 +1,38 @@
 CREATE DATABASE IF NOT EXISTS coffee_pos;
 USE coffee_pos;
 
+DROP TABLE IF EXISTS invoice_items;
+DROP TABLE IF EXISTS invoices;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS suppliers;
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    name VARCHAR(100),
+    username VARCHAR(50) UNIQUE,
+    password VARCHAR(255),
     role ENUM('admin','cashier') DEFAULT 'cashier',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users(name,username,password,role)
+VALUES('Admin','admin',MD5('123'),'admin');
+
+CREATE TABLE suppliers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    supplier_name VARCHAR(150),
+    phone VARCHAR(50),
+    address TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE products (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    product_name VARCHAR(150) NOT NULL,
-    sell_price DECIMAL(10,2) DEFAULT 0,
+    product_name VARCHAR(150),
+    category VARCHAR(100),
+    buy_price DECIMAL(10,2),
+    sell_price DECIMAL(10,2),
     stock INT DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -22,6 +41,8 @@ CREATE TABLE invoices (
     id INT AUTO_INCREMENT PRIMARY KEY,
     invoice_no VARCHAR(50),
     total DECIMAL(10,2),
+    paid DECIMAL(10,2),
+    balance DECIMAL(10,2),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
